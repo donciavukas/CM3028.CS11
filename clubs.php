d6d64308a0fc4a1dbcb26bd18a9950157471fafd
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-07 16:53:46 +0000

    Update clubs.php

--- clubs.php
+++ clubs.php
@@ -58,7 +58,7 @@
             <div class="form-group">
               <input type="password" placeholder="Password" class="form-control">
             </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
+            <button type="submit" class="btn btn-success"href="login.php">Sign in</button>
             <button type="submit" class="btn btn-success"> <a href="register.php">Register</a></button>
           </form>
         </div><!--/.navbar-collapse -->
