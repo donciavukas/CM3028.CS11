1a3f07652c63c8a1afd42abda9a5be87a2f602f6
Augustass <auguskairys@gmail.com>
2016-12-08 22:25:42 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -11,16 +11,16 @@ if(isset($_POST['sButton'])){
  
   $sql = "SELECT email FROM user WHERE (email = '$email')";
   $result = mysqli_query($db, $sql);
-  echo '$email'
+  echo '$email';
  
   if(($password == $password2)&&(mysqli_num_rows($result)>0)){
     $sql = "INSERT INTO user (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password')";
     mysqli_query($db, $sql);
-    echo 'mldc'
+    echo 'mldc';
     //header("Location: index.php");
   }else{
      $_SESSION['message'] = "The two passwords did not match";
-     echo 'ciulpk'
+     echo 'ciulpk';
      //header("Location: register.php");
   }
 }
