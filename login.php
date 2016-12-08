a2316bf21397e97d2f222a7e1bc2ce0bccb0a05e
Augustass <auguskairys@gmail.com>
2016-12-08 22:53:32 +0000

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 	$sql = "SELECT email, password FROM user WHERE (email = '$email') AND (password='$password')";
 	$result = mysqli_query($db,$sql);
 
-	if(!$row = mysqli_fetch_assoc($result)){
+	if(!$row = mysqli_fetch_assoc('$result')){
 		echo "Incorrect"
 	}else{
 		echo "Logged in"
