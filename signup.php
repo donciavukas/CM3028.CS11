8f46071be68934a916cbbd38361c4eb2b054696b
Augustass <auguskairys@gmail.com>
2016-12-08 19:43:21 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,8 +13,8 @@ if(isset($_POST['sButton'])){
   $result = mysqli_query($db, $sql1);
   
   if(($password == $password2)&&(mysqli_num_rows($result)=0)){
-    //$sql = "INSERT INTO user (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password')";
-    //mysqli_query($db, $sql);
+    $sql = "INSERT INTO user (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password')";
+    mysqli_query($db, $sql);
     $_SESSION['message'] = "You are now logged in";
     $_SESSION['email'] = $email;
     header("Location: index.php");
