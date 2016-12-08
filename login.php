ca6828aad6a9a0b43f837871bbc6130ac49acc23
Augustass <auguskairys@gmail.com>
2016-12-08 22:50:50 +0000

    Update login.php

--- login.php
+++ login.php
@@ -4,10 +4,10 @@
 	$email = $_POST ['inputEmail'];
 	$password = $_POST ['inputPassword'];
 
-	$sql = "SELECT email user WHERE email = $email AND password=$password";
+	$sql = "SELECT email, password FROM user WHERE (email = '$email') AND (password='$password')";
 	$result = mysqli_query($db,$sql);
 
-	if(!$row == mysqli_fetch_assoc($result)){
+	if(!$row = mysqli_fetch_assoc($result)){
 		echo "Incorrect"
 	}else{
 		echo "Logged in"
