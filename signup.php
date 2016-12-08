fe2c70095e77a4e77feca7bb80ac354ce9718709
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 16:20:09 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,12 +2,14 @@
 session_start();
 include 'DBCONNECT.php';
 
+mysql_select_db("web_data", $db);
+
 $first = $_POST['inputName'];
 $last = $_POST['inputSurname'];
 $email = $_POST['inputEmail'];
 $password = $_POST['inputPassword'];
 
-/$sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last', '$email', '$password')"
+$sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last', '$email', '$password')"
 //$sql = "INSERT INTO user (id, first, last, email, password) VALUES(1,'hi', 'bye', 'hello@gmail.com', '123')"
 //  $sql = 'INSERT INTO user '.
  //     '(first, last, email, password) '.
