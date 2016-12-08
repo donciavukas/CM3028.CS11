43db26fbd16956f0fe0a2b3987936c44f9e05192
Augustass <auguskairys@gmail.com>
2016-12-08 13:58:20 +0000

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include 'DBCONNECT.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last', '$email', '$password')"
+$result = mysql_query($db, $sql);
+header("Location: index.php");
+?>
