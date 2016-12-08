56ea25f3f732f128d661463885cb2aae51b8ab80
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 17:01:44 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -16,6 +16,20 @@ $sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last'
 
 mysqli_query($db, $sql);
 
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if (mysqli_query($db, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
 /*
 mysqli_query($db,"SELECT * FROM users");
 mysqli_query($db,"INSERT INTO users (first,last,email, password) 
