6e34729fdc10d7030207ff990d519ec80481a14a
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 17:53:40 +0000

    Update register.php

--- register.php
+++ register.php
@@ -92,12 +92,12 @@ session_start();
         <input type="password" class="form-control" name="inputPasswordConfirm" data-match="#inputPassword" data-match-error="Whoops, these don't match" placeholder="Confirm" required>
         <div class="help-block with-errors"></div>
         
-        
+        <!
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
         <label for="inputPassword2" class="sr-only">Re-enter Password</label>
         <input type="password" name="inputPassword2" class="form-control" placeholder="Re-enter Password" required>
-        
+        >
         <div class="checkbox">
           <label>
             <input type="checkbox" value="iAgree"> I Agree to the terms of service.
