0fe8b22a180ac2493a59856135cb2dc6ed018453
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 17:11:34 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,19 +8,21 @@ $email = $_POST['inputEmail'];
 $password = $_POST['inputPassword'];
 
 
-$sql = "INSERT INTO user (first, last, email, password) VALUES ('$first', '$last', '$email', '$password')"
+$sql = "INSERT INTO user (first, last, email, password) VALUES ('Donce','Storas', 'lel@gmail.com', 'hue')"; 
+
+//$sql = "INSERT INTO user (first, last, email, password) VALUES ('$first', '$last', '$email', '$password')"
 //$sql = "INSERT INTO user (id, first, last, email, password) VALUES(1,'hi', 'bye', 'hello@gmail.com', '123')"
 //  $sql = 'INSERT INTO user '.
  //     '(first, last, email, password) '.
   //    'VALUES (inputName, inputSurname, inputEmail, inputPassword)';
 
-mysqli_query($db, $sql);
-/*
+//mysqli_query($db, $sql);
+
 if (mysqli_query($db, $sql)) {
     echo "success";
 } else {
     echo "Error creating table: " . mysqli_error($conn);
-}*/
+}
 
 /*
 mysqli_query($db,"SELECT * FROM users");
