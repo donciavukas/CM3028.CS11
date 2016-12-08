4d8f866647f88478845a1d07ad65d8f58012d2e6
Augustass <auguskairys@gmail.com>
2016-12-08 14:13:14 +0000

    Update DBCONNECT.php

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b516a86d1fb393", "e5b1ec77", "web_data");
-
+/*
 if (!$db) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
@@ -12,5 +12,6 @@ echo "Success: A proper connection to MySQL was made! The my_db database is grea
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($db);
+*/
 ?>
 
