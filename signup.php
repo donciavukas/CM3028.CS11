2f30c29bf2d426ef61ad4ebe2c4052fca42fc442
Augustass <auguskairys@gmail.com>
2016-12-08 19:52:28 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,11 +9,11 @@ if(isset($_POST['sButton'])){
   $email = mysql_real_escape_string($_POST['inputEmail']);
   $password = mysql_real_escape_string($_POST['inputPassword']);
   $password2 = mysql_real_escape_string($_POST['inputPassword2']);
-  
+  echo $firstName;
+  /*
   $sql1 = "SELECT email FROM user WHERE email = '$email'";
   $result = mysqli_query($db, $sql1);
-  echo mysqli_num_rows($result);
- /* 
+ 
   if(($password == $password2)&&(mysqli_num_rows($result)=0)){
     $sql = "INSERT INTO user (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password')";
     mysqli_query($db, $sql);
