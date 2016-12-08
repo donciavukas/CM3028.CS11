78bdab6ee3abdcd5cb76c39585a17a46478768ae
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 15:38:26 +0000

    Update DBCONNECT.php

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -1,9 +1,9 @@
 <?php
-$db = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b516a86d1fb393", "e5b1ec77", "web_data");
+$db = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b516a86d1fb393", "", "web_data");
 
-if (!$db) {
-    die("Connection failed: ".mysqli_connection_error());
-}
+//if (!$db) {
+//    die("Connection failed: ".mysqli_connection_error());
+//}
 
 ?>
 
