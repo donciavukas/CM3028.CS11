94f747cfac0359fca66088bdd11b5579e8c93109
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 15:24:05 +0000

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,9 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
   <head>
+     <?php
+    include("dbconnect.php")
+      ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
