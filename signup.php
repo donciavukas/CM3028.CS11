918a28341dcd1752c335dfe93dd9550478acdbc7
donciavukas <d.gruzinskas@rgu.ac.uk>
2016-12-08 16:12:17 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,10 +8,10 @@ $email = $_POST['inputEmail'];
 $password = $_POST['inputPassword'];
 
 //$sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last', '$email', '$password')"
-$sql = "INSERT INTO user (id, first, last, email, password) VALUES(1,'hi', 'bye', 'hello@gmail.com', '123')"
-//  $sql = 'INSERT INTO user '.
-//      '(first, last, email, password) '.
-//      'VALUES (inputName, inputSurname, inputEmail, inputPassword)';
+//$sql = "INSERT INTO user (id, first, last, email, password) VALUES(1,'hi', 'bye', 'hello@gmail.com', '123')"
+  $sql = 'INSERT INTO user '.
+      '(first, last, email, password) '.
+      'VALUES (inputName, inputSurname, inputEmail, inputPassword)';
 $result = mysqli_query($db,$sql);
 header("Location: index.php");
 
