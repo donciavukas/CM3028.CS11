bc7f5c7206c475f2ae9a3199b0738645553f7a6b
Augustass <auguskairys@gmail.com>
2016-12-08 15:10:14 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,14 +7,7 @@ $last = $_POST['inputSurname'];
 $email = $_POST['inputEmail'];
 $password = $_POST['inputPassword'];
 
-echo $first;
-echo $last;
-echo $email;
-echo $password;
-
-  
-/*
 $sql = "INSERT INTO user (first, last, email, password) VALUES('$first', '$last', '$email', '$password')"
 $result = mysql_query($db, $sql);
 header("Location: index.php");
-*/
+
