5ff9bbcd3a94169db7cb5ca332ef52220c6f6771
Augustass <auguskairys@gmail.com>
2016-12-08 23:17:05 +0000

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
 	$email = $_POST ['inputEmail'];
 	$password = $_POST ['inputPassword'];
 
-	$sql = "SELECT email, password FROM user WHERE (email = '$email') AND (password='$password')";
-	$result = mysqli_query($db,$sql);
+	//$sql = "SELECT email, password FROM user WHERE (email = '$email') AND (password='$password')";
+	//$result = mysqli_query($db,$sql);
 /*
 	if(!$row = mysqli_fetch_assoc('$result')){
 		echo "Incorrect";
