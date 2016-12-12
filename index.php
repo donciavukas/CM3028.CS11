cfe285314ed3dece3d07301962e0b43844d526aa
Augustass <auguskairys@gmail.com>
2016-12-12 16:53:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -49,11 +49,14 @@
         
        
           <div class="form-group" id = "loginTest">
-          <?php if(1=1){?>
-            
-          <?php }else{ ?>
-
-          <?php } ?>
+          <form class="navbar-form navbar-right" action="login.php" method="POST">    
+              <input type="text" placeholder="Email" class="form-control" name = "inputEmail">    
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control" name = "inputPassword">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+            <a class="btn btn-default" href="register.php">Register</a>
+          </form>
           </div>
       </div>
     </nav>
